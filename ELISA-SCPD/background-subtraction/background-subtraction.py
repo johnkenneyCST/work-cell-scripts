923b8331a9dab1f38d4ff101f48e3a3516b43ca1
JohnKenney <john.kenney@cellsignal.com>
2024-09-06 10:29:17 -0400

    Updates and new script

---
+++ ELISA-SCPD/background-subtraction/background-subtraction.py
@@ -0,0 +1,120 @@
+import argparse
+import csv 
+import openpyxl 
+import pandas as pd
+
+parser = argparse.ArgumentParser(prog="background-subtraction.py")
+parser.add_argument('--file', type=str, required=True)
+args = parser.parse_args()
+
+# These columns are the columns where the degen peptides will be
+DEGEN_PEPTIDE_COLS = ["21","22","23"]
+
+# These are the columns where the regular peptides will go
+REG_PEPTIDE_COLS = ["1","2","3",
+                    "5", "6","7",
+                    "9","10","11",
+                    "13","14","15",
+                    "17","18","19"]
+
+# These are the rows where all peptides will go 
+PEPTIDE_ROWS = ["A","C","E","G","I","K","M","O"]
+
+PEPTIDE_GROUPS = {1:["1","2","3"],
+                  2: ["5", "6","7"],
+                  3: ["9","10","11"],
+                  4:["13","14","15"],
+                  5: ["17","18","19"]}
+
+class file_handler:
+    def __init__(self, file) -> None:
+        self.file = file
+        self.read_file()
+
+    def read_file(self):
+        self.meta_data = pd.read_excel(self.file)
+        barcodes = self.meta_data[self.meta_data['Unnamed: 1'].str.contains("EAP", na=False)]
+        barcode_list = barcodes['Unnamed: 1'].tolist()
+        index_list = barcodes["Unnamed: 1"].index.tolist()
+        self.plate_list = {}
+        for i in range(len(barcode_list)):
+            self.plate_list[f"{barcode_list[i]}"] = self.plate(file_intance=self,barcode=barcode_list[i], start_index=index_list[i])
+
+
+    class plate:
+        def __init__(self, file_intance, barcode, start_index) -> None:
+            self.file_handler = file_intance
+            self.barcode = barcode
+            self.start_index = start_index + 4
+        
+            self.collect_wells()
+            self.subtract_degen()
+            
+
+        def collect_wells(self):
+            self.wells = self.file_handler.meta_data[self.start_index:self.start_index+384]
+            self.wells_list = self.wells["Unnamed: 1"].tolist()
+            self.wells_list_data = self.wells["Unnamed: 2"].tolist()
+            
+            self.well_data_list = []
+            for i in range(len(self.wells_list)):
+                self.well_data_list.append(self.well(self, self.wells_list[i], self.wells_list_data[i]))
+        
+        def subtract_degen(self):
+            self.normal_peptides = []
+            self.degen_peptides = {}
+            for i in self.well_data_list:
+                if i.is_degen:
+                    self.degen_peptides[i.well] = i
+                elif i.is_reg_peptide:
+                    self.normal_peptides.append(i)
+                else:
+                    continue
+            
+            for i in self.normal_peptides:
+                i.new_value = round(i.value - self.degen_peptides[i.degen_peptide_to_sub].value, 4)
+                print(f"New Value: {i.new_value} -> Old Value: {i.value} Subtracted: {self.degen_peptides[i.degen_peptide_to_sub].value}")
+        
+        class well:
+            def __init__(self, plate_instance, well, value) -> None:
+                self.plate_instance = plate_instance
+                self.well = well 
+                self.value = value
+                self.is_degen = False
+                self.is_reg_peptide = False
+                self.new_value = None
+
+                # print(self.well, self.value, self.plate_instance.barcode)
+                self.determine_if_degen()
+                self.determine_if_regular_peptide()
+
+                if self.is_reg_peptide:
+                    self.find_corresponding_degen_peptide()
+
+            def determine_if_degen(self):
+                # This function will determine whether this well is a degenerate peptide well, since the location of the degen peptide will always reside in the same cols and rows we can calculate this
+
+                tmpRowLetter = self.well[:1]
+                tmpColNumber = self.well[1:]
+                
+                if tmpRowLetter in PEPTIDE_ROWS and tmpColNumber in DEGEN_PEPTIDE_COLS:
+                    self.is_degen = True
+
+            def determine_if_regular_peptide(self):
+                tmpRowLetter = self.well[:1]
+                tmpColNumber = self.well[1:]
+                
+                if tmpRowLetter in PEPTIDE_ROWS and tmpColNumber not in DEGEN_PEPTIDE_COLS and tmpColNumber in REG_PEPTIDE_COLS:
+                    self.is_reg_peptide = True
+
+            def find_corresponding_degen_peptide(self):
+                # This function will be called if a peptide is not a degen, this will find the degen peptide to be subtracted from. 
+                tmpColNumber = int(self.well[1:])
+                for k, v in PEPTIDE_GROUPS.items():
+                    if str(tmpColNumber) in v:
+                        self.degen_peptide_index = v.index(str(tmpColNumber))
+                        self.degen_peptide_to_sub = f"{self.well[:1]}{DEGEN_PEPTIDE_COLS[self.degen_peptide_index]}"
+
+
+if __name__ == "__main__":
+    handler  = file_handler(args.file)
--- ELISA-SCPD/peptide-link/peptide-link.py
+++ ELISA-SCPD/peptide-link/peptide-link.py
@@ -22,9 +22,61 @@ def load_configs():
     else:
          return configs
 
+class PeptideHandler:
+    def __init__(self, st_issues, config) -> None:
+        self.st_numbers = st_issues
+        self.config = config
+        self.st_dict = {}
+        self.makeRequest()
 
+    def makeRequest(self):
+        for i in range(len(self.st_numbers)):
+            self.st_dict[self.st_numbers[i]] = []
+        
+        for i in self.st_numbers:
+            try: 
+                request = requests.get(url=self.config['barcode-retrieval']['jira-scpd-details-url-prod']+i)
+            except Exception as e:
+                exit(e)
+            else:
+                print(i)
+                for j in request.json()['List']:
+                    self.st_dict[i].append(ST_Issue_Entry(j['ID'], j["ISSUENUM"], j['MONOCLONALPROJECT'], j['ANIMAL'], 
+                                                          j['NUMBEROFSAMPLES'], j['PEPTIDENAMES'],j['PLATEBARCODE'], 
+                                                          j['EAPBARCODE'], j['PEPTIDERESERVOIR']))
+                    
+
+
+class ST_Issue_Entry:
+    """ 
+    This class is for storing the TGE information from JIRA. A user is given a set of ST numbers, using the API we get those numbers and the data is passed 
+    to this class. 
+
+    Each instance of this class represents a single row in the TGE.
+    """
+    def __init__(self, 
+                 id,
+                 issue_num,
+                 mono_proj_num,
+                 animal_num,
+                 num_of_samples,
+                 peptide_names,
+                 tfw_barcode,
+                 eap_barcode,
+                 peptide_reservoir) -> None:
+        
+        self.id = id
+        self.issue_num = issue_num
+        self.mono_proj_num = mono_proj_num.split(" ")[0]
+        self.animal_num = animal_num
+        self.num_of_samples = num_of_samples
+        self.peptide_names = peptide_names.split("<")[0].strip()
+        self.tfw_barcode = tfw_barcode
+        self.eap_barcode = eap_barcode
+        self.peptide_reservoir_barcode = peptide_reservoir
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    st_issues = 
+    st_issues = args.st_issues.split(" ")
     config = load_configs()
+    pep_handler = PeptideHandler(st_issues, config)
